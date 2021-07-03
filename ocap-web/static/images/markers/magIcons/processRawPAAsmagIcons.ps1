f56ad2ba72f1b4fa464de30a6313e2baf0d9adf8
IndigoFox <indifox926@gmail.com>
2021-07-03 19:55:19 -0400

    magIcons from IFA, RHS, ACE, vanilla + DLC
    Faces of War is obfuscated
    Enoch DLC is encrypted

--- ocap-web/static/images/markers/magIcons/processRawPAAsmagIcons.ps1
+++
@@ -1,9 +0,0 @@
-ForEach ($file in (Get-ChildItem -File | ForEach-Object name)) {
-	New-Item -Path ".\magIcons" -Name "${file}" -Type Directory -Force
-	Copy-Item -Path ".\$file" -Destination ".\magIcons\${file}" -Force -EA SilentlyContinue
-	Rename-Item -Path ".\magIcons\${file}\${file}" -NewName "FFFFFF.paa" -Force -EA SilentlyContinue
-	. "K:\SteamLibrary\steamapps\common\Arma 3 Tools\TexView2\Pal2PacE.exe" ".\magIcons\${file}\FFFFFF.paa" ".\magIcons\${file}\FFFFFF.png"
-	Copy-Item ".\magIcons\${file}\FFFFFF.png" ".\magIcons\${file}\000000.png"
-	Copy-Item ".\magIcons\${file}\FFFFFF.png" ".\magIcons\${file}\EDEBBA.png"
-	Remove-Item -Path ".\magIcons\${file}\*.paa" -Force -EA SilentlyContinue
-}
